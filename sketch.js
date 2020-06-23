fecceacab12803ab6d169de3108ba0dc4d578c46
rafaelima99 <43426335+rafaelima99@users.noreply.github.com>
2020-06-23 03:06:15 -0500

    refactor: var name change

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-let imagemCenario;
-const protagonista = {
+const Hipsta = {
   sprite: "",
   width: 110,
   height: 135,
@@ -13,11 +12,13 @@ const gotinha = {
 };
 
 let cenario;
+let imagemCenario;
+let protagonista;
 let trilhaSonora;
 
 function preload() {
   imagemCenario = loadImage("imagens/cenario/floresta.png");
-  protagonista.sprite = loadImage("imagens/personagem/correndo.png");
+  Hipsta.sprite = loadImage("imagens/personagem/correndo.png");
   trilhaSonora = loadSound("sons/trilha_jogo.mp3");
 }
 
@@ -26,14 +27,14 @@ function setup() {
 
   cenario = new Cenario(imagemCenario, 2);
 
-  Hipsta = new Personagem(
-    protagonista.sprite,
-    protagonista.spriteWidth,
-    protagonista.spriteHeight,
-    protagonista.width,
-    protagonista.height,
-    protagonista.rows,
-    protagonista.cols
+  personagem = new Personagem(
+    Hipsta.sprite,
+    Hipsta.spriteWidth,
+    Hipsta.spriteHeight,
+    Hipsta.width,
+    Hipsta.height,
+    Hipsta.rows,
+    Hipsta.cols
   );
 
   frameRate(32);
@@ -44,5 +45,5 @@ function draw() {
   cenario.show();
   cenario.move();
 
-  Hipsta.show();
+  personagem.show();
 }
